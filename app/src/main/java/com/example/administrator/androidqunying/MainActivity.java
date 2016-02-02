ad96cc2c6fe7146f3e3cd30952c7f29c6cf42da1
a1510312433 <1510312433@qq.com>
2016-02-02 15:07:49 +0800

    解决警告

---
+++ app/src/main/java/com/example/administrator/androidqunying/MainActivity.java
@@ -0,0 +1,192 @@
+package com.example.administrator.androidqunying;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView lv;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+
+
+    //    private ListView lv ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+//        lv = (ListView)findViewById(R.id.listView);
+        lv = ((ListView) findViewById(R.id.listView));
+        lv.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_UP:
+                        System.out.println("ACTION_UP");
+                        break;
+                    case MotionEvent.ACTION_DOWN:
+                        System.out.println("ACTION_DOWN");
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+//                        System.out.println("ACTION_MOVE");
+                        break;
+                }
+                return false;
+            }
+        });
+
+        lv.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+                switch (scrollState) {
+                    case SCROLL_STATE_FLING:
+                        System.out.println("SCROLL_STATE_FLING");
+                        break;
+                    case SCROLL_STATE_IDLE:
+                        System.out.println("SCROLL_STATE_IDLE");
+                        break;
+                    case SCROLL_STATE_TOUCH_SCROLL:
+                        System.out.println("SCROLL_STATE_TOUCH_SCROLL");
+                        break;
+                }
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem,
+                                 int visibleItemCount, int totalItemCount) {
+                if (firstVisibleItem + visibleItemCount == totalItemCount && totalItemCount > 0) {
+                    System.out.println("到达了最后一行");
+                }
+
+                if (firstVisibleItem > lastvisibleitemposition) {
+                    System.out.println("在上滑");
+                } else if (firstVisibleItem < lastvisibleitemposition) {
+                    System.out.println("在下滑");
+                }
+                lastvisibleitemposition = firstVisibleItem;
+
+                System.out.println("lastposition" + lv.getLastVisiblePosition());
+                System.out.println("firstposition" + lv.getFirstVisiblePosition());
+            }
+        });
+        lv.setAdapter(new myAdapter());
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+    }
+
+    int lastvisibleitemposition;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+
+    }
+
+    private class myAdapter extends BaseAdapter {
+        LayoutInflater inflater;
+
+        public myAdapter() {
+            inflater = LayoutInflater.from(MainActivity.this);
+        }
+
+        @Override
+        public int getCount() {
+            return 100;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if (convertView == null) {
+                holder = new ViewHolder();
+                convertView = inflater.inflate(R.layout.item, null);
+                holder.txt = ((TextView) convertView.findViewById(R.id.textView2));
+                convertView.setTag(holder);
+            } else {
+                holder = ((ViewHolder) convertView.getTag());
+            }
+            holder.txt.setText("---" + position + "---");
+            return convertView;
+        }
+    }
+
+    private class ViewHolder {
+        private TextView txt;
+    }
+
+
+}
